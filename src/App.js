5617f427b5377eeca044dc0d440eefe9175e81c2
Muhammad Raffiq Mohd Khalil <muhammadraffiq@gmail.com>
2021-07-25 23:58:00 +0800

    part 3

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 //import Setup from './tutorial/1-useState/setup/2-useState-basics';
 //import Final from './tutorial/1-useState/final/2-useState-basics';
 
-import Setup from './tutorial/1-useState/setup/3-useState-array';
+//import Setup from './tutorial/1-useState/setup/3-useState-array';
+import Setup from './tutorial/1-useState/setup/4-useState-object';
 
 function App() {
   return (
